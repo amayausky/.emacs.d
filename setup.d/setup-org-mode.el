ea99280436d5b233b9f56290807aa23588f8eb33
Aaron Mayausky <aaron.mayausky@gmail.com>
2019-07-22 13:19:25 -0400

    add org agenda files

--- setup.d/setup-org-mode.el
+++ setup.d/setup-org-mode.el
@@ -9,5 +9,8 @@
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cb" 'org-iswitchb)
 
+(setq org-agenda-files (quote ("~/org"
+			       "~/org/wbgb")))
+
 (provide 'setup-org-mode)
 ;;; setup-org-mode ends here
