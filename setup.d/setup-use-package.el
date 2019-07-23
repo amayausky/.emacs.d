b6c90f6896174c632371ea73755be8ca6818739a
Aaron Mayausky <aaron.mayausky@gmail.com>
2019-07-23 18:07:33 -0400

    add autoupdate to use-package

--- setup.d/setup-use-package.el
+++ setup.d/setup-use-package.el
@@ -13,5 +13,11 @@
 
 (setq use-package-always-ensure t)
 
+(use-package auto-package-update
+  :config
+  (setq auto-package-update-delete-old-versions t)
+  (setq auto-package-update-hide-results t)
+  (auto-package-update-maybe))
+
 (provide 'setup-use-package)
 ;;; setup-use-package ends here
