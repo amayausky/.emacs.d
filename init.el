eea3d55aaa9f3cc24ed95c49e93b4518febb70e0
Aaron Mayausky <amayausky@wbgames.com>
2020-12-04 17:33:12 -0500

    more cleanup

--- init.el
+++ init.el
@@ -14,7 +14,6 @@
 
 ;; load and setup packages here
 (require 'setup-nlinum)
-(require 'setup-diminish)
 (require 'setup-exec-path-from-shell)
 (require 'setup-theme)
 (require 'setup-flycheck)
@@ -31,21 +30,8 @@
 (require 'setup-projectile)
 (require 'setup-ace-window)
 (require 'setup-org-mode)
-(require 'setup-plantuml-mode)
 
 (provide 'init)
 ;;; init.el ends here
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(plantuml-mode ace-jump-mode ace-window projectile counsel ivy magit json-mode yaml-mode go-rename go-guru rats go-errcheck go-eldoc company-go company-lsp lsp-ui lsp-mode company yasnippet flycheck-color-mode-line flycheck abyss-theme exec-path-from-shell diminish nlinum auto-package-update use-package)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
