18f5c41e9521a19bb2483839206ea3731ee81d61
Aaron Mayausky <amayausky@wbgames.com>
2021-07-12 17:23:07 -0400

    markdown

--- setup.d/setup-markdown-mode.el
+++ setup.d/setup-markdown-mode.el
@@ -5,7 +5,12 @@
 ;;; Code:
 
 (use-package markdown-mode
-  :ensure t)
+  :ensure t
+  :commands (markdown-mode gfm-mode)
+  :mode (("README\\.md\\'" . gfm-mode)
+	 ("\\.md\\'" . markdown-mode)
+	 ("\\.markdown\\'" . markdown-mode))
+  :init (setq markdown-command "multimarkdown"))
 
 (provide 'setup-markdown-mode)
 ;;; setup-markdown-mode ends here
