e791f53a8a2a96a96210a092b8713a573b2406f0
Aaron Mayausky <amayausky@wbgames.com>
2021-07-21 14:16:22 -0400

    add compile command for go-mode

--- setup.d/setup-go-mode.el
+++ setup.d/setup-go-mode.el
@@ -7,6 +7,12 @@
 (use-package go-mode
   :ensure t)
 
+(defun go-mode-setup ()
+  (setq compile-command "echo Building... && go build -v && echo Testing... && go test -v && echo Vetting... go vet && echo Linter... && staticcheck -fail none")
+  (setq compilation-read-command nil)
+  (local-set-key (kbd "C-c c") 'compile))
+(add-hook `go-mode-hook `go-mode-setup)
+
 (add-hook 'go-mode-hook 'enable-tabs)
 
 (provide 'setup-go-mode)
