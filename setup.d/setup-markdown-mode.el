2d58e4dafcf18d8846266ffe33bc06eb0143e074
Aaron Mayausky <aaron.mayausky@gmail.com>
2021-07-21 20:56:29 -0400

    change markdown-command to markdown

--- setup.d/setup-markdown-mode.el
+++ setup.d/setup-markdown-mode.el
@@ -10,7 +10,7 @@
   :mode (("README\\.md\\'" . gfm-mode)
 	 ("\\.md\\'" . markdown-mode)
 	 ("\\.markdown\\'" . markdown-mode))
-  :init (setq markdown-command "multimarkdown"))
+  :init (setq markdown-command "markdown"))
 
 (provide 'setup-markdown-mode)
 ;;; setup-markdown-mode ends here
